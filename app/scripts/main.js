06170c3fc711a6b5519edb2e4b0b149c3a59fd2a
Hoetmaaiers <houdmeyers@gmail.com>
2014-09-07 11:58:18 +0200

    add normalise.css & svginjector

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+// console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+
+  // Elements to inject
+  var mySVGsToInject = document.querySelectorAll('img.svg-inject');
+
+  // Do the injection
+  SVGInjector(mySVGsToInject);
+
+});
